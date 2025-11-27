bab82c922b7e4bd41922aaaac4f99a56be281b81
k4hr <alecsandr-7a@yandex.ru>
2025-11-27 17:47:57 +0300

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,10 @@
 // src/routes/index.ts
 import express from 'express'
 import receiveWebhook from '../controllers/webhook/receive-webhook'
+
 const router = express.Router()
-router.post('/webhook', receiveWebhook)
+
+// только вебхук
+router.use('/webhook', receiveWebhook)
+
 export default router
