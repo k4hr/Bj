667fa5312028aebdd2e87d1592a91f3f95c35fe6
k4hr <alecsandr-7a@yandex.ru>
2025-11-27 18:13:15 +0300

    Add setWebhook route to index.ts
    Added route for setting Telegram webhook.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,14 @@
 // src/routes/index.ts
 import express from 'express'
 import receiveWebhook from '../controllers/webhook/receive-webhook'
+import setWebhook from '../controllers/webhook/set-webhook'
 
 const router = express.Router()
 
-// только вебхук от Telegram
+// один раз дергаем руками, чтобы зарегистрировать вебхук у Telegram
+router.get('/set-webhook', setWebhook)
+
+// сюда Telegram будет слать апдейты (method: POST)
 router.post('/webhook', receiveWebhook)
 
 export default router
