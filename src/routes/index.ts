2937e56e9b91fbb40b82c508ae37971838eb2033
k4hr <alecsandr-7a@yandex.ru>
2025-11-27 17:17:54 +0300

    Refactor webhook route handling

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,6 @@
+// src/routes/index.ts
 import express from 'express'
-import testBot from '../controllers/test-bot/hello-user'
 import receiveWebhook from '../controllers/webhook/receive-webhook'
-
 const router = express.Router()
-
-// Only for test route
-router.use('/test', testBot)
-
-// Use in local test for set telegram webhook
-router.use('/webhook', receiveWebhook)
-
+router.post('/webhook', receiveWebhook)
 export default router
